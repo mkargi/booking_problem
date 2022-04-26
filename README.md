a015e38cb13a6bc9cdb162da7b7e9ce6e6ee175c
uidj1709 <matthias.karg@continental-corporation.com>
2022-04-26 18:34:41 +0200

    do precheck and sort bookings

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ The goal is to assign all bookings to a free room.
 Output looks like this:
 
 ```sh
-❯ ./build/booking_problem 
-room0:CAAXXX-DDDD-------------------
-room1:BBBBBEE-----------------------
-room2:XXXX--------------------------
-room3:-DDD--------------------------
-room4:GG-FFFF-----------------------
+❯ build/booking_problem
+counter 64
+room0:CAAXXXFFFF--------------------
+room1:BBBBBBBNNMMMM-----------------
+room2:XXXXXJJPPOOOO-----------------
+room3:QDDDKKLLIII-------------------
+room4:HHGGGEEE-ZZZZZZZ--------------
 ```
--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <set>
+#include <map>
+#include <algorithm>
 #include "booking.h"
 #include "room.h"
 
 using namespace bp;
 
+bool preCheck( Booking::List& bookings, int maxRooms )
+{
+    std::sort(bookings.begin(), bookings.end(), [](const Booking& b0, const Booking& b1){ return b0.start < b1.start; } );
+    std::map<int, int> counter;
 
+    for( const auto& booking : bookings )
+        for( int i=booking.start; i<=booking.end; i++)
+            counter[i]++;
+
+    for( const auto& [key,val] : counter)
+        if( val > maxRooms )
+            return false;
+
+    return true;
+}
 
 bool assignBookings( Booking::List bookings, Booking::List openBookings, Room::List rooms, int bn, int rn, std::set<Room::List>& output, bool stopByFirstMatch = false )
 {
+    static int counter = 0; 
+    counter++;
     bool status = false;
 
     if( rn >= rooms.size() )
@@ -16,7 +34,8 @@ bool assignBookings( Booking::List bookings, Booking::List openBookings, Room::L
         if( bookings.empty() )
         {
             output.insert(rooms);
-            status = true;
+            status = true;            
+            std::cout << "counter " << counter << std::endl << std::flush;
         }            
     }
     else if( bn >= bookings.size() )
@@ -25,9 +44,9 @@ bool assignBookings( Booking::List bookings, Booking::List openBookings, Room::L
     }
     else
     {  
+        //std::cout << "rn: " << rn << " bn: " << bn << std::endl;
         for( int i=bn; i<bookings.size(); i++)
-        {
-            //std::cout << "bn " << bn << " rn " << rn << std::endl;
+        {      
             std::swap( bookings[bn], bookings[i] );
             bool isOverlapping = rooms[rn].isOverlapping(bookings[bn]);
 
@@ -61,22 +80,35 @@ int main(int argc, char *argv[])
     
     // add fixed bookings, which cannot be shifted
     rooms.at(0).bookings.push_back(Booking(0, 3, 5, 'X', false));
-    rooms.at(2).bookings.push_back(Booking(1, 0, 3, 'X', false));
+    rooms.at(2).bookings.push_back(Booking(1, 0, 4, 'X', false));
 
     // define some bookings
-    Booking::List bookings = { Booking(2, 1, 2, 'A', true)
-                             , Booking(3, 0, 4, 'B', true)
-                             , Booking(4, 0, 0, 'C', true)
-                             , Booking(5, 1, 3, 'D', true)
-                             , Booking(6, 5, 6, 'E', true)
-                             , Booking(7, 7, 10, 'D', true)
-                             , Booking(8, 3, 6, 'F', true)
-                             , Booking(9, 0, 1, 'G', true)
+    Booking::List bookings = { Booking(2,   1,  2,  'A',    true)
+                             , Booking(3,   0,  6,  'B',    true)
+                             , Booking(4,   0,  0,  'C',    true)
+                             , Booking(5,   1,  3,  'D',    true)
+                             , Booking(6,   5,  7,  'E',    true)
+                             , Booking(7,   6,  9,  'F',    true)
+                             , Booking(8,   2,  4,  'G',    true)
+                             , Booking(9,   0,  1,  'H',    true)
+                             , Booking(10,  8, 10,  'I',    true)
+                             , Booking(11,  5,  6,  'J',    true)
+                             , Booking(12,  4,  5,  'K',    true)
+                             , Booking(13,  6,  7,  'L',    true)
+                             , Booking(14,  9, 12,  'M',    true)
+                             , Booking(15,  7,  8,  'N',    true)
+                             , Booking(16,  9, 12,  'O',    true)
+                             , Booking(17,  7,  8,  'P',    true)
+                             , Booking(18,  0,  0,  'Q',    true)
+                             , Booking(19,  9,  15, 'Z',    true)
                             };
 
     // assign bookings to rooms
-    std::set<Room::List> output;
-    assignBookings( bookings, {}, rooms, 0, 0, output, true );
+    std::set<Room::List> output;    
+    if( preCheck( bookings, rooms.size() ) )
+        assignBookings( bookings, {}, rooms, 0, 0, output, true );
+    else
+        std::cout << "too many bookings" << std::endl;
 
     // print all possible solutions
     for( const auto& out : output )
